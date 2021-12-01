e0e69292b84dfffb76af1a220c0a783c65a5f5cf
root <presto3477@gmail.com>
2021-12-01 16:35:46 +0000

    chore: save unsaved file

--- processor.py
+++ processor.py
@@ -14,7 +14,7 @@ def preprocess_function(examples, tokenizer, data_args):
         labels = tokenizer(targets, max_length=max_target_length, padding=padding, truncation=True)
 
     # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
-    # padding in the loss.
+    # padding in the loss.s
     if padding == "max_length" and data_args.ignore_pad_token_for_loss:
         labels["input_ids"] = [
             [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
