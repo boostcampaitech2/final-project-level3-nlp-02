c031f8607169f3db8fe5f8cad7dc56b4f5f82527
sangha <sangha110495@gmail.com>
2021-12-02 17:21:11 +0000

    Update tokenizer.py

--- tokenizer.py
+++ tokenizer.py
@@ -4,17 +4,18 @@ import json
 import pandas as pd
 from transformers import AutoTokenizer
 
-
 class ToknierOptimization :
     def __init__(self, dir_path) :
         self.dir_path = dir_path
 
     def load_txt(self, path) : 
+        assert path.endswith('.txt')
         with open(path, "r") as f :
             data = f.read()
         return data
 
     def write_txt(self, vocab_list, path) :
+        assert path.endswith('.txt')
         data_size = len(vocab_list)
         f = open(path, 'w')
         for i in range(data_size):
@@ -22,26 +23,18 @@ class ToknierOptimization :
         f.close()
 
     def load_json(self, path) :
+        assert path.endswith('.json')
         with open(path, "r") as f:
             data = json.load(f)
         return data
 
     def write_json(self, tokenizer_data, path) :
+        assert path.endswith('.json')
         with open(path, 'w') as f:
             json.dump(tokenizer_data, f)
 
-
-class BertTokenizerOptimization(ToknierOptimization) :
-    def __init__(self, dir_path) :
-        super().__init__(dir_path)
-        self.tokenizer_path = os.path.join(dir_path, 'tokenizer.json')
-        self.vocab_path = os.path.join(dir_path, 'vocab.txt')
-
     # update vocab mapping
-    def update_unused(self, idx2vocab, ch_df) :
-        unused_start = 31500
-        unused_size = 300
-        
+    def update_unused(self, idx2vocab, ch_df, unused_start, unused_size, prefix) :        
         size = 0
         map_id = 0
         while(size < unused_size) :
@@ -57,16 +50,30 @@ class BertTokenizerOptimization(ToknierOptimization) :
                     continue
 
                 idx2vocab[target_id] = ch
-                idx2vocab[target_id+1] = '##' + ch
+                idx2vocab[target_id+1] = prefix + ch
                 size += 2
                 
             map_id += 1
 
         return idx2vocab
 
+    def load_csv(self, csv_path) :
+        assert csv_path.endswith('.csv')
+        df = pd.read_csv(csv_path)        
+        if 'Character' not in df.columns() or 'KoreanFlag' not in df.columns() :
+            raise KeyError ('Wrong Key Name in DataFrame')
+        return df
+
+
+class BertTokenizerOptimization(ToknierOptimization) :
+    def __init__(self, dir_path) :
+        super().__init__(dir_path)
+        self.tokenizer_path = os.path.join(dir_path, 'tokenizer.json')
+        self.vocab_path = os.path.join(dir_path, 'vocab.txt')
+
     # optimizing tokenizer
     def optimize(self, extra_ch_path) :
-        ch_df = pd.read_csv(extra_ch_path)
+        ch_df = self.load_csv(extra_ch_path)
 
         # load tokenizer data
         tokenizer_data = self.load_json(self.tokenizer_path)
@@ -74,7 +81,7 @@ class BertTokenizerOptimization(ToknierOptimization) :
         idx2vocab = {i:vocab for i, vocab in enumerate(vocab_data.split('\n')[:-1])}
         
         # update tokenizer
-        idx2vocab = self.update_unused(idx2vocab, ch_df)
+        idx2vocab = self.update_unused(idx2vocab, ch_df, 31500, 500, '##')
         vocab_list = list(idx2vocab.values())
         vocab2idx = {idx2vocab[key] : key for key in idx2vocab.keys()}
         tokenizer_data['model']['vocab'] = vocab2idx
@@ -94,33 +101,6 @@ class BartTokenizerOptimization(ToknierOptimization) :
         self.tokenizer_path = os.path.join(dir_path, 'tokenizer.json')
         self.vocab_path = os.path.join(dir_path, 'vocab.json')
 
-    # update vocab mapping
-    def update_unused(self, idx2vocab, ch_df) :
-        unused_start = 7
-        unused_size = 100
-        
-        size = 0
-        map_id = 0
-        while(size < unused_size) :
-            ch, flag = ch_df.iloc[map_id][['Character', 'KoreanFlag']]
-
-            target_id = unused_start + size
-            if flag == False :
-                idx2vocab[target_id] = ch
-                size += 1
-            else :
-                if size + 2 >= unused_size :
-                    map_id += 1
-                    continue
-
-                idx2vocab[target_id] = ch
-                idx2vocab[target_id+1] = '▁' + ch
-                size += 2
-                
-            map_id += 1
-
-        return idx2vocab
-
     def update_added_tokens(self, tokenizer_data, vocab_list) :
         unused_start = 7
         unused_size = 100
@@ -130,14 +110,14 @@ class BartTokenizerOptimization(ToknierOptimization) :
 
     # optimizing tokenizer
     def optimize(self, extra_ch_path) :
-        ch_df = pd.read_csv(extra_ch_path)
+        ch_df = self.load_csv(extra_ch_path)
 
         # load tokenizer data
         tokenizer_data = self.load_json(self.tokenizer_path)
         vocab2idx = self.load_json(self.vocab_path)
         idx2vocab = {idx:vocab for vocab,idx in vocab2idx.items()}
         # update tokenizer
-        idx2vocab = self.update_unused(idx2vocab, ch_df)
+        idx2vocab = self.update_unused(idx2vocab, ch_df, 7, 100, '▁')
         vocab2idx = {idx2vocab[key] : key for key in idx2vocab.keys()}
         vocab_list = list(idx2vocab.values())
         
