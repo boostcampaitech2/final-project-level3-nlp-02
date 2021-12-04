28a382910291e3dd8e07786e478ce9ce5e3a2a63
abbymark <abbymark@naver.com>
2021-12-04 13:48:23 +0000

    backup for gpu problem

--- dataloader.py
+++ dataloader.py
@@ -4,6 +4,11 @@ from torch.utils.data import Dataset
 
 
 from datasets import load_dataset, concatenate_datasets
+import os
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+
+api_token = os.getenv('HF_DATASET_API_TOKEN')
 
 
 class SumDataset(Dataset) :
@@ -14,7 +19,7 @@ class SumDataset(Dataset) :
         self.dataset = []
         self.mode=mode
         for data_type in data_types :
-            self.dataset.append(load_dataset(data_type, use_auth_token=True))
+            self.dataset.append(load_dataset(data_type, use_auth_token=api_token))
     def load_data(self):
         dataset = concatenate_datasets([ds[self.mode] for ds in self.dataset])
         return dataset
