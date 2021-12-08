98b40ba9ffc04c6a5fcfbeb6be134aa71c79d61a
root <presto3477@gmail.com>
2021-12-08 05:35:57 +0000

    test: reproducibility test

--- running.sh
+++ running.sh
@@ -2,17 +2,34 @@
 python train.py \
 --do_train \
 --dataset_name paper,news,magazine \
---output_dir model/kobart-v1 \
+--output_dir model/es_test \
 --num_train_epochs 2 \
 --learning_rate 3e-05 \
 --max_source_length 1024 \
 --max_target_length 128 \
 --metric_for_best_model rougeLsum \
---es_patience 3 \
 --relative_eval_steps 10 \
---relative_sample_ratio 0.5 \
---project_name kobart_1207 \
---wandb_unique_tag kobartV1_ep2_lr3e05_srclen1024_R50
+--es_patience 2 \
+--load_best_model_at_end True \
+--relative_sample_ratio 0.001 \
+--project_name reproducibility_test \
+--wandb_unique_tag cache_test_model_init2
+
+
+# python train.py \
+# --do_train \
+# --dataset_name paper,news,magazine \
+# --output_dir model/kobart-v1 \
+# --num_train_epochs 2 \
+# --learning_rate 3e-05 \
+# --max_source_length 1024 \
+# --max_target_length 128 \
+# --metric_for_best_model rougeLsum \
+# --es_patience 3 \
+# --relative_eval_steps 10 \
+# --relative_sample_ratio 0.5 \
+# --project_name kobart_1207 \
+# --wandb_unique_tag kobartV1_ep2_lr3e05_srclen1024_R50
 
 # python train.py \
 # --do_eval \
