4d64e73678508fc8ae9bb6583298fcc03f01714f
changyong93 <ghy546@naver.com>
2021-12-17 08:47:52 +0000

    update shell script

--- model/running.sh
+++ model/running.sh
@@ -32,7 +32,7 @@ python pretrain.py \
 --adam_beta1  0.9 \
 --adam_beta2  0.98 \
 --adam_epsilon 1e-06 \
---use_doc_type_ids True
+# --use_doc_type_ids True
 
 
 # 1. h_dim 128/256 => 논문 => 128 / 256
@@ -49,23 +49,23 @@ python pretrain.py \
 ## 시도해볼 부분: epoch 수정해보기
 ## 변경 필요한 arguments: output_dir
 
-python train.py \
---do_train \
---output_dir checkpoint/rdroptest_nolabelsmoothed \
---num_train_epochs 100 \
---learning_rate 3e-05 \
---max_source_length 1024 \
---max_target_length 128 \
---metric_for_best_model rougeLsum \
---relative_eval_steps 10 \
---es_patience 3 \
---load_best_model_at_end True \
---project_name baseV1.0_Kobart \
---wandb_unique_tag rdroptest_nolabelsmoothed \
---save_total_limit 3 \
---num_samples 10 \
---overwrite_output_dir \
---use_rdrop True
+# python train.py \
+# --do_train \
+# --output_dir checkpoint/rdroptest_nolabelsmoothed \
+# --num_train_epochs 100 \
+# --learning_rate 3e-05 \
+# --max_source_length 1024 \
+# --max_target_length 128 \
+# --metric_for_best_model rougeLsum \
+# --relative_eval_steps 10 \
+# --es_patience 3 \
+# --load_best_model_at_end True \
+# --project_name baseV1.0_Kobart \
+# --wandb_unique_tag rdroptest_nolabelsmoothed \
+# --save_total_limit 3 \
+# --num_samples 10 \
+# --overwrite_output_dir \
+# --use_rdrop True
 # --label_smoothing_factor 0.1 # BART rdrop 사용시 필수
 
 # python train.py \
@@ -127,15 +127,7 @@ python train.py \
 # --is_noam True \
 # --warmup_steps 2000
 
-<<<<<<< HEAD
 python predict.py \
 --model_name_or_path checkpoint/baseV1.0_Kobart \
 --num_beams 3
-=======
-
-# python predict.py \
-# --model_name_or_path /opt/ml/final_project/model/checkpoint/kobigbirdbart \
-# --tokenizer_name monologg/kobigbird-bert-base \
-# --num_beams 3 \
->>>>>>> dev
 # --use_model bigbart
