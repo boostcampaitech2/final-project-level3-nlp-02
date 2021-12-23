b4784b5344032555f311371a217417240522ed6a
root <presto3477@gmail.com>
2021-12-23 06:10:28 +0000

    chore: shift_to_right fix for test

--- model/test.py
+++ model/test.py
@@ -53,7 +53,7 @@ def main():
         (ModelArguments, DataTrainingArguments, LoggingArguments, CustomSeq2SeqTrainingArguments)
     )
     model_args, data_args, log_args, training_args = parser.parse_args_into_dataclasses()    
-    breakpoint()
+    
     seed_everything(training_args.seed)
     assert training_args.do_eval
     training_args.predict_with_generate = True
@@ -64,7 +64,7 @@ def main():
     load_dotenv(dotenv_path=data_args.use_auth_token_path)
     USE_AUTH_TOKEN = os.getenv("USE_AUTH_TOKEN")    
     test_dataset = load_dataset('metamong1/summarization', split="test", use_auth_token=USE_AUTH_TOKEN)
-    test_dataset = test_dataset.select(range(30))
+    test_dataset = test_dataset.select(range(5))
 
     if data_args.use_preprocessing:
         data_preprocessor = Preprocessor()
@@ -142,7 +142,6 @@ def main():
         if training_args.generation_max_length is not None
         else data_args.val_max_target_length)
     results = {}
-    breakpoint()
     num_beams = data_args.num_beams if data_args.num_beams is not None else training_args.generation_num_beams
     metrics = trainer.evaluate(max_length=max_length, num_beams=num_beams, metric_key_prefix="eval")
     print("#########Eval metrics: #########", metrics) 
